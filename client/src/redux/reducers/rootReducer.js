ad02fc0ed3aff5f9fd2a78eba4c6dd8a30021e0f
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-09-27 22:15:21 -0300

    config redux

---
+++ client/src/redux/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+// importar reducers
+
+const rootReducer = combineReducers({
+    // aca irian los nombres de los reducers
+});
+
+export default rootReducer;
+
---
+++ client/src/redux/store/index.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from '../reducer/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+);
+
+export default store;
