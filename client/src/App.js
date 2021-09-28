1fc5a071e32f0b7d89c9664ef00a23b6bb421bbb
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-09-28 15:57:51 -0300

    arreglo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 
 import "./App.css";
-=======
 import './App.css';
 import { Route } from "react-router-dom";
 
@@ -9,13 +8,15 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./components/Login/Login";
 import LogoutButton from "./components/Logout/Logout";
 import Profile from "./components/Profile/Profile";
+
 function App() {
   const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
       {isAuthenticated ? <LogoutButton /> : <LoginButton />}
       <Profile />
-      <Route path="products/:name" component={ProductsByName} />
+      <Route path="products/:name" />
+      {/* <Route path="products/:name" component={ProductsByName} /> */} 
     </div>
   );
 }
