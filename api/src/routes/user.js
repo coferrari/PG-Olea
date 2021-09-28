f754d1c4b0f6ce7f8e30066e40cafecffab74647
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-28 16:44:28 -0300

    modificamos las funciones que se usan en las rutas de usuarios

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -2,15 +2,15 @@ const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');const { Router } = require("express");
 
-const userControllers = require("../controllers/user");
+const userFunction = require("../controllers/user");
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get("/", userControllers.getAll);
-router.get("/:id", userControllers.getByID);
-router.post("/", userControllers.create);
-router.put("/:id", userControllers.update);
-router.delete("/:id", userControllers.delete);
+// router.get("/", userControllers.getAll);
+// router.get("/:id", userControllers.getByID);
+router.post("/signup", userFunction.signUp);
+// router.put("/:id", userControllers.update);
+// router.delete("/:id", userControllers.delete);
 
 module.exports = router;
