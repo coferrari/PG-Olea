3ee0023d0c7e22a83b653d841d0649f4832798e1
tomasgoyret <tomas.goyret@gmail.com>
2021-09-28 10:00:45 -0300

    creacion_rama

--- api/src/db.js
+++ api/src/db.js
@@ -34,6 +34,7 @@ const { Pokemon } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+//Push nueva rama
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
