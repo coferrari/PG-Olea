ef986273c64fe3c0d87adb5c0c102dbe6259bb25
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-09-29 12:39:41 -0300

    arreglo bug

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store/index";
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <App />
-        </Switch>
-      </BrowserRouter>
+
+    <Provider store= {store}>
+    <BrowserRouter>
+    <Switch>
+    <App />
+    </Switch>
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
