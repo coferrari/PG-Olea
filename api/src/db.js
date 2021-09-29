7046e2a914ad8618241003b2f3b451db67e0e85c
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-09-29 12:58:19 -0300

    agrego ternario a la conexion con la DB

--- api/src/db.js
+++ api/src/db.js
@@ -4,13 +4,16 @@ const fs = require("fs");
 const path = require("path");
 const { dbUser, dbPassword, dbHost, dbName } = require("./utils/config");
 
-const sequelize = new Sequelize(
-  `postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = dbPassword
+  ? new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`, {
+      logging: false, // set to console.log to see the raw SQL queries
+      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    })
+  : new Sequelize(`postgres://${dbUser}@${dbHost}/${dbName}`, {
+      logging: false,
+      native: false,
+    });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
