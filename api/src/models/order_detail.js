5d6e81ef4cf05c4a8b4592a7c65a8b01e9cf8a6a
nahuelkbx <nahueltomasmonserrat@gmail.com>
2021-09-28 21:55:59 -0300

    creacion de modelo order_detail y refactorización de order

---
+++ api/src/models/order_detail.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("order_detail", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
