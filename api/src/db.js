67658ddddbc8a55fde4d3e162ad97fa5b719328b
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-29 13:01:56 -0300

    se borra un espacio con el prettier

--- api/src/db.js
+++ api/src/db.js
@@ -50,7 +50,6 @@ Product.belongsTo(Brand);
 Category.hasMany(Product);
 Product.belongsTo(Category);
 
-
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
