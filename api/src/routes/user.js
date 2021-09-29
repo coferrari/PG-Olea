68a8c3f73a26ef93186003704179baf4b0b3e8e0
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-28 21:17:32 -0300

    añadimos la ruta post del login

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,15 +1,16 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');const { Router } = require("express");
-
-const userFunction = require("../controllers/user");
 const router = Router();
-
+const validate = require("../utils/middlewares/validateSchemaModel");
+const userFunction = require("../controllers/user");
+const schema = require("../utils/schemas/user.joi");
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 // router.get("/", userControllers.getAll);
 // router.get("/:id", userControllers.getByID);
-router.post("/signup", userFunction.signUp);
+router.post("/register", validate(schema), userFunction.register);
+router.post("/login", userFunction.login);
 // router.put("/:id", userControllers.update);
 // router.delete("/:id", userControllers.delete);
 
