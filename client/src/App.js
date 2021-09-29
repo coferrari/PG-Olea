837d126de319875b7b1100b4b642e3b7a66b217f
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-09-29 12:23:50 -0300

    remuevo autenticacion con Auth0 y agrego nuevo ruteo para login, logout y register' '

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
-
 import "./App.css";
-import './App.css';
+import "./App.css";
 import { Route } from "react-router-dom";
-
-
-import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./components/Login/Login";
 import LogoutButton from "./components/Logout/Logout";
 import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
+import Register from "./components/Register/Register";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
-      <Profile />
-      <Route path="products/:name" />
-      {/* <Route path="products/:name" component={ProductsByName} /> */} 
+      <Navbar />
+      <Route exact path="/login">
+        <LoginButton />
+      </Route>
+      <Route exact path="/register">
+        <Register />
+      </Route>
+      <Route exact path="/logout">
+        <LogoutButton />
+      </Route>
     </div>
   );
 }
