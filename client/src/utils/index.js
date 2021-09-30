990b2afecf34348f135c78be4844c9b6a4fddc54
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-30 10:42:48 -0300

    se añadieron 3 funciones para verificar si el usuario esta logeado o no

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,5 @@
 import jwt_decode from "jwt-decode";
 const getToken = () => localStorage.getItem("token");
-
 const isAuthorized = () => (getToken() ? true : false);
 const decodeToken = () => jwt_decode(getToken());
 
