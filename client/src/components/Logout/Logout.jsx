f27373944b858ff9e1c3a722371c4e59f07d319c
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-30 09:59:18 -0300

    implementamos la funcion que destruye el token y lo redirecciona

--- client/src/components/Logout/Logout.jsx
+++ client/src/components/Logout/Logout.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Link, useHistory } from "react-router-dom";
+import { logOut } from "../../auth/users";
 const LogoutButton = () => {
-
+  const history = useHistory();
+  const handleClick = (e) => {
+    e.preventDefault();
+    logOut();
+    history.push("/");
+  };
   return (
-    <button>
+    <button
+      onClick={(e) => {
+        handleClick(e);
+      }}
+    >
       Log Out
     </button>
   );
