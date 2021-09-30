f8a1d3991d84a63914b1ab0e3caa23b953935b2b
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-30 16:37:02 -0300

    modificamos las funciones que envian la informacion

--- client/src/auth/users.js
+++ client/src/auth/users.js
@@ -9,6 +9,7 @@ export const register = async (user) => {
 export const logIn = async (user) => {
   const token = await axios.post(`${baseUrl}/login`, user);
   localStorage.setItem("token", token.data.data.token);
+
   return token.data;
 };
 export const logOut = () => {
@@ -18,7 +19,9 @@ export const logInGoogle = async (response) => {
   const res = await axios.post(`${baseUrl}/googlelogin`, {
     token: response.tokenId,
   });
-  console.log(res);
+  localStorage.setItem("token", res.data.data.token);
+  console.log(res.data.data.token);
+  return res.data;
 };
 export const registerGoogle = async (response) => {
   const res = await axios.post(`${baseUrl}/googleregister`, {
