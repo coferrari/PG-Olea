357fcf4856b821635e212ac0c48faf0f2372c884
nahuelkbx <nahueltomasmonserrat@gmail.com>
2021-09-30 11:09:24 -0300

    se termina el css de los productos

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,30 +1,26 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProducts } from '../../redux/actions/index'; 
-import { Product } from '../Product/Product';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../redux/actions/index";
+import { Product } from "../Product/Product";
+import styles from "./Products.module.css";
 
 export default function Products() {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.productsReducer.products);
 
-    const dispatch = useDispatch();
-    const products = useSelector(state => state.productsReducer.products);
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
 
-    useEffect(() => {
-        dispatch(getProducts());
-    }, []);
-    
-    console.log(products);
-      
-    return (
-       <div>
-           {products?.map((p) => {
-               return <Product
-                id={p.id}
-                name={p.name}
-                image={p.image[0]}
-                price={p.price} 
-               /> 
-           })}
-       </div>
-    );
+  console.log(products);
+
+  return (
+    <div className={styles.container}>
+      {products?.map((p) => {
+        return (
+          <Product id={p.id} name={p.name} image={p.image[0]} price={p.price} />
+        );
+      })}
+    </div>
+  );
 }
