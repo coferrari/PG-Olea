52cc3e7db2946cc83aa5fcf759c1df011b5c060e
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-30 09:46:59 -0300

    cree archivo para usar en un futuro para la verificacion

---
+++ api/src/utils/middlewares/sendEmail.js
@@ -0,0 +1,12 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (options) => {
+//   let transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     secure: false,
+//       user: testAccount.user,
+//       pass: testAccount.pass,
+//     },
+//   });
+// };
