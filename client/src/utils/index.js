0551e62461aa83f5a7c5bc3ba6bf0dce3021bdc7
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-30 09:46:19 -0300

    hicimos los dispatch al back, recibimos el token y generamos el usuario

---
+++ client/src/utils/index.js
@@ -0,0 +1,7 @@
+import jwt_decode from "jwt-decode";
+const getToken = () => localStorage.getItem("token");
+
+const isAuthorized = () => (getToken() ? true : false);
+const decodeToken = () => jwt_decode(getToken());
+
+export { getToken, isAuthorized, decodeToken };
