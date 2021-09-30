3a2034f3b44775ab8275bc2db29912291ffe9175
dylangavilan <dylan.gavilan32@gmail.com>
2021-09-30 10:00:29 -0300

    se añadio un ternario, si estas logeado solo ves logout, sino register o login

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import Register from '../Register/Register';
-
+import Register from "../Register/Register";
+import { isAuthorized, decodeToken } from "../../utils/index";
 const Navbar = () => {
+  const validate = isAuthorized();
+  if (validate) {
+    const user = decodeToken();
     return (
-        <div>
-            OLEA
-            <ul>
-            <Link to="/login"><li>Login</li></Link>
-            <Link to="/register"><li>Register</li></Link>
-            <Link to="/logout"><li>Logout</li></Link>
-            </ul>
-            
-        </div>
+      <div>
+        <h1>hola {user.username}</h1>
+        OLEA
+        <ul>
+          <Link to="/logout">
+            <li>Logout</li>
+          </Link>
+        </ul>
+      </div>
     );
-}
- 
+  }
+  return (
+    <div>
+      OLEA
+      <ul>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+        <Link to="/register">
+          <li>Register</li>
+        </Link>
+        <Link to="/logout">
+          <li>Logout</li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
 export default Navbar;
