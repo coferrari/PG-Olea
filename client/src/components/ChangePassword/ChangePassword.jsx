5ad844bed88b3ee8a1ceecb79b201a5a5f4ad324
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-10-01 14:11:04 -0300

    componente changePassword

--- client/src/components/ChangePassword/ChangePassword.jsx
+++ client/src/components/ChangePassword/ChangePassword.jsx
@@ -1,43 +1,63 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import {requestChangePassword} from "../../auth/users";
+import { changePassword } from "../../auth/users";
 import style from "./ChangePassword.module.css";
 
-export function validate(email) {
-  let errors = "";
-  if (!/\S+@\S+\.\S+/.test(email)) {
-    errors = "Email is invalid";
+export function validate(input) {
+  let errors = {};
+  if (!input.email) {
+    errors.email = "Email is required";
+  } else if (!/\S+@\S+\.\S+/.test(input.email)) {
+    errors.email = "Email is invalid";
+  } else if (!input.passwordOne) {
+    errors.passwordOne = "Password is required";
+  } else if (
+    !/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(input.passwordOne)
+  ) {
+    errors.passwordOne = "Password is invalid";
+  } else if (!input.passwordTwo) {
+    errors.passwordTwo = "Password is required";
+  } else if (input.passwordOne !== input.passwordTwo) {
+    errors.passwordTwo = "Password must much";
   }
   return errors;
 }
 
 const ChangePassword = () => {
-  const [email, setEmail] = useState();
-  const [errors, setErrors] = useState();
+  const [input, setInput] = useState({
+    email: "",
+    passwordOne: "",
+    passwordTwo: "",
+  });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await requestChangePassword(email);
-    } catch (err) {
-      throw new Error(err);
-    }
+  const [errors, setErrors] = useState({});
 
-  };
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log('entra al submit de cambio de contraseña')
+      changePassword(input.email, input.passwordTwo)
+  }
 
   const handleChange = (e) => {
-    e.preventDefault();
-    setEmail(e.target.value);
-    setErrors(validate(e.target.value));
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
   };
 
   return (
     <div className="container">
       <div className="col-lg-4 mx-auto text-center">
         <Form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
         >
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
@@ -45,19 +65,56 @@ const ChangePassword = () => {
               type="email"
               placeholder="Enter email"
               name="email"
-              value={email}
+              value={input.email}
               onChange={(e) => {
                 handleChange(e);
               }}
             />
-            {errors && <div className={style.errors}>{errors}</div>}
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            {errors.email && <div className={style.errors}>{errors.email}</div>}
           </Form.Group>
-
-          {!errors && email && (
-            <Button variant="dark" type="submit">
-              Solicitar cambio de contraseña
-            </Button>
-          )}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              type="password"
+              placeholder="Password"
+              name="passwordOne"
+              value={input.passwordOne}
+            />
+            {errors.passwordOne && (
+              <div className={style.errors}>{errors.passwordOne}</div>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Repeti tu password</Form.Label>
+            <Form.Control
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              type="password"
+              placeholder="Password"
+              name="passwordTwo"
+              value={input.passwordTwo}
+            />
+            {errors.passwordTwo && (
+              <div className={style.errors}>{errors.passwordTwo}</div>
+            )}
+          </Form.Group>
+          {input.email &&
+            input.passwordOne &&
+            input.passwordTwo &&
+            !errors.email &&
+            !errors.passwordOne &&
+            !errors.passwordTwo && (
+              <Button variant="dark" type="submit">
+                Confirmar cambio de contraseña
+              </Button>
+            )}
         </Form>
       </div>
     </div>
