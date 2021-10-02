432b76fd44dfb922c3f37f788c639874750b48dc
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-02 14:42:47 -0300

    se agrego el user el redirect

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { useParams } from "react-router";
-import { useHistory } from "react-router-dom";
+import { useHistory, Redirect } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { getToken, decodeToken } from "../../utils/index";
 import CreateProduct from "../CreateProduct/CreateProduct";
 
 export default function UserInfo() {
+  const loggedIn = decodeToken();
+  console.log(loggedIn);
   return (
     <div>
       <h1>Users</h1>
-
-      <CreateProduct />
+      {loggedIn.admin ? <CreateProduct /> : <Redirect to="/home" />}
     </div>
   );
 }
