0139182ebb18adf57947ff756002bd24369b9b21
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-01 23:00:01 -0300

    se agregaron middlewares

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,23 +1,21 @@
 const { Router } = require("express");
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');const { Router } = require("express");
 const router = Router();
 const validate = require("../utils/middlewares/validateSchemaModel");
 const userFunction = require("../controllers/user");
 const schema = require("../utils/schemas/user.joi");
 const verifyToken = require("../utils/middlewares/validateToken");
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-router.get("/", verifyToken, userFunction.getAll);
-// router.get("/:id", userControllers.getByID);
-router.post("/register", userFunction.register);
-router.post("/login", userFunction.login);
+const {
+  checkDuplicate,
+  checkEmailAndPassword,
+} = require("../utils/middlewares/checkDuplicate");
+const isAdmin = require("../utils/middlewares/isAdmin");
+router.get("/", isAdmin, verifyToken, userFunction.getAll);
+router.post("/register", checkDuplicate, userFunction.register);
+router.post("/login", checkEmailAndPassword, userFunction.login);
 router.put("/", verifyToken, userFunction.changePassword);
 router.put("/changepassword", userFunction.changePassword);
 router.post("/googlelogin", userFunction.googleLogin);
-
 router.post("/confirmregister", userFunction.confirmRegister);
 router.post("/requestchangepassword", userFunction.requestChangePassword);
-// router.delete("/:id", userControllers.delete);
 
 module.exports = router;
