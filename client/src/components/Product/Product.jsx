43a9141ae52895413ec69e8cd99a7dbe6edd0901
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-10-02 15:50:24 -0300

    boton carrito

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Product.module.css";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import Spinner from "react-bootstrap/Spinner";
+import {
+  addProductsToChart,
+  removeProductsFromChart,
+} from "../../redux/actions/index";
+import { useDispatch, useSelector } from "react-redux";
+
 export function Product({ id, name, image, price }) {
+  const productsChart = useSelector(
+    (state) => state.carritoReducer.productsCarrito
+  );
+
+  const isInChart = productsChart.find((product) => product.id === id);
+
+  const dispatch = useDispatch();
+
+  const handleAddToChart = (e) => {
+    e.preventDefault();
+    dispatch(addProductsToChart({ id }));
+  };
+
+  const handleRemoveFromChart = (e) => {
+    e.preventDefault();
+    dispatch(removeProductsFromChart(id));
+  };
+
   return (
     // definir qué info mostrar
     <div className={styles.container}>
@@ -16,6 +40,24 @@ export function Product({ id, name, image, price }) {
             </Link>
             <Card.Text>{price}</Card.Text>
           </Card.Body>
+
+          {isInChart ? (
+            <Button
+              variant="secondary"
+              type="submit"
+              onClick={(e) => handleRemoveFromChart(e)}
+            >
+              Eliminar del carrito
+            </Button>
+          ) : (
+            <Button
+              variant="dark"
+              type="submit"
+              onClick={(e) => handleAddToChart(e)}
+            >
+              Agregar al carrito
+            </Button>
+          )}
         </Card>
       ) : (
         <Spinner animation="border" />
