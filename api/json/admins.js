523da728f392b2b30e17f710d17eebf2ee5c1ed7
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-02 14:38:11 -0300

    agregamos los mockups de admins

--- api/json/admins.js
+++ api/json/admins.js
@@ -2,13 +2,13 @@ const admins = [
   {
     username: "dylan",
     email: "dylans55@hotmail.com",
-    password: "123",
+    password: "123Dylan",
     admin: true,
   },
   {
     username: "joaco",
     email: "joaquinjoby@gmail.com",
-    password: "123",
+    password: "123Joaco",
     admin: true,
   },
 ];
