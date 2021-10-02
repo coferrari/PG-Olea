c09d9ffa670fde2c39700096a364afec2bec9b30
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-02 20:00:20 -0300

    se agrego el middleware para que solo los admin hagan post productos

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,20 +1,19 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');const { Router } = require("express");
-
+const verifyToken = require("../utils/middlewares/validateToken");
+const isAdmin = require("../utils/middlewares/isAdmin");
 const productControllers = require("../controllers/product");
 const router = Router();
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 router.get("/", productControllers.getAll);
 router.get("/search/:id", productControllers.getByID);
 router.get("/orderByPrice/:price", productControllers.orderByPrice);
 router.get("/orderByName/:name", productControllers.orderByName);
 router.get("/orderByRating/:rating", productControllers.orderByRating);
+router.post("/create", isAdmin, productControllers.create);
 router.post("/", productControllers.create);
 router.post("/add/carrito", productControllers.addProduct);
-router.put("/:id", productControllers.update);
-router.delete("/:id", productControllers.delete);
+router.put("/:id", isAdmin, productControllers.update);
+router.delete("/:id", isAdmin, productControllers.delete);
 
 module.exports = router;
