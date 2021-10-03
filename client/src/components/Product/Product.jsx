6b8578144b25dca03d05c06603564e8a62a452e8
Constanza Ferrari <ferrariconstanza@gmail.com>
2021-10-02 21:50:01 -0300

    agregar/sacar producto del carrito con local storage

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Product.module.css";
 import { Card, Button } from "react-bootstrap";
@@ -9,56 +9,59 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 
 export function Product({ id, name, image, price }) {
-  const productsChart = useSelector(
+  const productsCart = useSelector(
     (state) => state.carritoReducer.productsCarrito
   );
 
-  const nameItemStorage = localStorage.getItem(name);
+  const isInStore = productsCart?.filter((product) => product.id == id);
+
+  const quantity = 1;
 
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(productsCart));
+  }, [productsCart]);
+
   const handleAddToChart = (e) => {
     e.preventDefault();
-    dispatch(addProductsToChart({ id }));
-    localStorage.setItem(name, id);
+    dispatch(addProductsToChart({ id, name, image, price, quantity }));
   };
 
   const handleRemoveFromChart = (e) => {
     e.preventDefault();
     dispatch(removeProductsFromChart(id));
-    localStorage.removeItem(name);
   };
 
   return (
     // definir qué info mostrar
     <div className={styles.container}>
-        <Card style={{ width: "30rem" }}>
-          <Card.Img variant="top" src={image ? image : ""} alt="producto" />
-          <Card.Body>
-            <Link to={`/product/${id}`}>
-              <Card.Title>{name}</Card.Title>
-            </Link>
-            <Card.Text>{price}</Card.Text>
-          </Card.Body>
-          {!nameItemStorage && (
-            <Button
-              variant="dark"
-              type="submit"
-              onClick={(e) => handleAddToChart(e)}
-            >
-              Agregar al carrito
-            </Button>
-          )}
-          {nameItemStorage && (
-            <Button
-              variant="secondary"
-              type="submit"
-              onClick={(e) => handleRemoveFromChart(e)}
-            >
-              Eliminar del carrito
-            </Button>
-          )}
-        </Card>
+      <Card style={{ width: "30rem" }}>
+        <Card.Img variant="top" src={image ? image : ""} alt="producto" />
+        <Card.Body>
+          <Link to={`/product/${id}`}>
+            <Card.Title>{name}</Card.Title>
+          </Link>
+          <Card.Text>{price}</Card.Text>
+        </Card.Body>
+        {!isInStore.length ? (
+          <Button
+            variant="dark"
+            type="submit"
+            onClick={(e) => handleAddToChart(e)}
+          >
+            Agregar al carrito
+          </Button>
+        ) : (
+          <Button
+            variant="secondary"
+            type="submit"
+            onClick={(e) => handleRemoveFromChart(e)}
+          >
+            Eliminar del carrito
+          </Button>
+        )}
+      </Card>
     </div>
   );
 }
