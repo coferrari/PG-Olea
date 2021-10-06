c1b7c88df881884a7cb757affbecd015c2a8aa71
leokatsikaris <leonelkatsikaris@gmail.com>
2021-10-06 13:08:37 -0300

    reducer wishlist creado

---
+++ client/src/redux/reducers/wishlistReducer.js
@@ -0,0 +1,22 @@
+import {
+    GET_WISHLIST,
+  } from "../actions/types";
+
+  const initialState = {
+    wishlist: [],
+  };
+  
+  export default function wishlistReducer(state = initialState, action) {
+    switch (action.type) {
+  
+      case GET_WISHLIST:
+        console.log(action.payload);
+        return {
+          ...state,
+          wishlist: [...state.wishlist, action.payload]
+        };
+
+      default:
+        return state;
+    }
+  }
