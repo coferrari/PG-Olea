2ac4013153549ec79735d8b582f52cd514886f74
nahuelkbx <nahueltomasmonserrat@gmail.com>
2021-10-07 19:25:24 -0300

    asd

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import store from "./redux/store/index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import dotenv from "dotenv";
-import { BrowserRouter } from "react-router-dom";
+
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
