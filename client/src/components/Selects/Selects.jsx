4a6241a3d2ec670c7af0f3da5e2c1cd0afe697e8
nahuelkbx <nahueltomasmonserrat@gmail.com>
2021-10-07 14:05:19 -0300

    fix bug

--- client/src/components/Selects/Selects.jsx
+++ client/src/components/Selects/Selects.jsx
@@ -6,16 +6,9 @@ import { getCategories } from "../../redux/actions";
 export default function Selects() {
   const dispatch = useDispatch();
   const history = useHistory();
-  // const categories = useSelector((state) => state.categoryReducer.categories);
-
   useEffect(() => {
     dispatch(getCategories());
   }, [dispatch]);
-
-  // function handleCategorySelect(name) {
-  //   history.push(`/category/${name}`);
-  // }
-
   const handleOrderSelect = function (order) {
     order = order.split(" ");
     history.push(`/home/${order[0]}/${order[1]}`);
@@ -23,19 +16,7 @@ export default function Selects() {
 
   return (
     <div>
-      {/* <select onChange={(e) => handleCategorySelect(e.target.value)}>
-        <option disabled selected hidden>
-          Category...
-        </option>
-        {categories?.map((c) => (
-          <option value={c.nameCategory}>{c.nameCategory}</option>
-        ))}
-      </select> */}
       <select onChange={(e) => handleOrderSelect(e.target.value)}>
-        {/* <option disabled selected hidden> */}
-        {/* <option disabled defaultValue="Ordernar">
-        Ordernar
-        </option> */}
         <option value={"name asc"}>Order by name: A-Z</option>
         <option value={"name desc"}>Order by name: Z-A</option>
         <option
