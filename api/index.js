5f6b04309efb584b33f8e689939818b3546cac60
nahuelkbx <nahueltomasmonserrat@gmail.com>
2021-10-06 21:48:00 -0300

    dep

--- api/index.js
+++ api/index.js
@@ -30,26 +30,26 @@ const { DB_URL } = process.env;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   // //Posteo todas las marcas y categorias
-  for (let i = 0; i < marcas.length; i++) {
-    const post = axios.post(`${DB_URL}/api/brand/`, marcas[i]);
-    post.then();
-  }
-  for (let i = 0; i < categorias.length; i++) {
-    const post = axios.post(`${DB_URL}/api/category/`, categorias[i]);
-    post.then();
-  }
+  // for (let i = 0; i < marcas.length; i++) {
+  //   const post = axios.post(`${DB_URL}/api/brand/`, marcas[i]);
+  //   post.then();
+  // }
+  // for (let i = 0; i < categorias.length; i++) {
+  //   const post = axios.post(`${DB_URL}/api/category/`, categorias[i]);
+  //   post.then();
+  // }
 
-  for (let j = 0; j < 5; j++) {
-    for (let i = 0; i < productos.length; i++) {
-      const post = axios.post(`${DB_URL}/api/product/create/`, productos[i]);
-      post.then();
-    }
-  }
+  // for (let j = 0; j < 5; j++) {
+  //   for (let i = 0; i < productos.length; i++) {
+  //     const post = axios.post(`${DB_URL}/api/product/create/`, productos[i]);
+  //     post.then();
+  //   }
+  // }
 
-  for (let i = 0; i < admin.length; i++) {
-    const post = axios.post(`${DB_URL}/api/user/createadmin/`, admin[i]);
-    post.then();
-  }
+  // for (let i = 0; i < admin.length; i++) {
+  //   const post = axios.post(`${DB_URL}/api/user/createadmin/`, admin[i]);
+  //   post.then();
+  // }
   server.listen(process.env.PORT || 3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
