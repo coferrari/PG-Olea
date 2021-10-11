15965235cf7fe4f4d1ea6155ffd28cc9a3491df0
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-11 19:24:53 -0300

    .

--- api/index.js
+++ api/index.js
@@ -3,16 +3,19 @@ const { conn } = require("./src/db.js");
 const marcas = require("./json/marcas.js");
 const categoryMockUp = require("./json/categorias");
 const productMockUp = require("./json/productos");
-const { adminMockUp } = require("./json/admins");
+const { adminMockUp, userMockUp } = require("./json/admins");
+const ordersMockUp = require("./json/ordernes");
 const product = require("./src/models/product.js");
 const { DB_URL } = process.env;
 conn
   .sync({ force: true })
   .then(async () => {
     //Posteo todas las marcas y categorias
+    await userMockUp();
+    await adminMockUp();
     await categoryMockUp();
     await productMockUp();
-    await adminMockUp();
+    // await ordersMockUp();
     await server.listen(process.env.PORT || 3001, () => {
       console.log("%s listening at 3001"); // eslint-disable-line no-console
     });
--- api/json/ordernes.js
+++ api/json/ordernes.js
@@ -1,11 +1,54 @@
-var obj = {
-  username: joaco,
-  price: 1200,
-  products: [
-    { id: 1, quantity: 4 },
-    { id: 2, quantity: 3 },
-    { id: 3, quantity: 3 },
-  ],
-  addresNum: 421,
-  addres: lujan,
+const { Order } = require("../src/db");
+
+const ordersMockUp = async () => {
+  try {
+    await Order.create({
+      username: "user1",
+      price: 1200,
+      products: [
+        { id: 1, quantity: 4 },
+        { id: 2, quantity: 3 },
+        { id: 3, quantity: 3 },
+      ],
+      addresNum: 421,
+      addres: "lujan",
+    });
+    await Order.create({
+      username: "user2",
+      price: 1500,
+      products: [
+        { id: 1, quantity: 4 },
+        { id: 2, quantity: 3 },
+        { id: 3, quantity: 3 },
+      ],
+      status: "procesando",
+      addresNum: 421,
+      addres: "lujan",
+    });
+    await Order.create({
+      username: "user3",
+      price: 1400,
+      products: [
+        { id: 1, quantity: 2 },
+        { id: 2, quantity: 3 },
+        { id: 3, quantity: 3 },
+      ],
+      addresNum: 421,
+      addres: "lujan",
+    });
+    await Order.create({
+      userUsername: "user2",
+      price: 3000,
+      products: [
+        { id: 1, quantity: 2 },
+        { id: 2, quantity: 3 },
+        { id: 3, quantity: 3 },
+      ],
+      addresNum: 421,
+      addres: "lujan",
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
+module.exports = ordersMockUp;
