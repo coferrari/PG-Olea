7caa5f31a6a66161fcb23eb1dbcb0173cde56a5a
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-11 15:34:28 -0300

    :

--- api/index.js
+++ api/index.js
@@ -1,41 +1,23 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const marcas = require("./json/marcas.js");
-const categorias = require("./json/categorias");
-const productos = require("./json/productos");
-const admin = require("./json/admins");
-const axios = require("axios");
+const categoryMockUp = require("./json/categorias");
+const productMockUp = require("./json/productos");
 const { adminMockUp } = require("./json/admins");
 const { User } = require("./src/db");
+const product = require("./src/models/product.js");
 const { DB_URL } = process.env;
 // Syncing all the models at once.
 
-conn.sync({ force: false }).then(() => {
-  //Posteo todas las marcas y categorias
-  // for (let i = 0; i < categorias.length; i++) {
-  //   const post = axios.post(
-  //     `https://olea-deploy.herokuapp.com/api/category/`,
-  //     categorias[i]
-  //   );
-  //   post.then();
-  // }
-
-  // for (let i = 0; i < productos.length; i++) {
-  //   const post = axios.post(
-  //     `https://olea-deploy.herokuapp.com/api/product/create/`,
-  //     productos[i]
-  //   );
-  //   post.then();
-  // }
-
-  // for (let i = 0; i < admin.length; i++) {
-  //   const post = axios.post(
-  //     `http://localhost:3001/api/user/createadmin/`,
-  //     admin[i]
-  //   );
-  //   post.then();
-  // }
-  server.listen(process.env.PORT || 3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
-  });
-});
+conn
+  .sync({ force: true })
+  .then(async () => {
+    //Posteo todas las marcas y categorias
+    await categoryMockUp();
+    await productMockUp();
+    await adminMockUp();
+    await server.listen(process.env.PORT || 3001, () => {
+      console.log("%s listening at 3001"); // eslint-disable-line no-console
+    });
+  })
+  .catch((e) => console.log(e.message));
--- api/json/admins.js
+++ api/json/admins.js
@@ -2,7 +2,6 @@ const bcrypt = require("bcrypt");
 const { User } = require("../src/db");
 
 const userMockUp = async () => {
-  // --- Users ---
   const hashedPasswordA = await bcrypt.hash("123Usuario", 12);
   try {
     await User.create({
@@ -12,23 +11,32 @@ const userMockUp = async () => {
       password: hashedPasswordA,
       contact: "02201236969",
       coins: 10,
+      admin: true,
     });
   } catch (e) {
     console.log(e.message);
   }
 };
-
 const adminMockUp = async () => {
-  // --- Admin ---
   const hashedPasswordB = await bcrypt.hash("Henry2021", 12);
   try {
     await User.create({
+      username: "olea",
       name: "Olea",
       surname: "Proyecto",
-      username: "olea",
       isAdmin: true,
       email: "oleaproyecto@gmail.com",
       password: hashedPasswordB,
+      admin: true,
+    });
+    await User.create({
+      username: "dylan",
+      name: "Dylan",
+      surname: "Gavilan",
+      isAdmin: true,
+      email: "dylans55@hotmail.com",
+      password: hashedPasswordB,
+      admin: true,
     });
   } catch (e) {
     console.log(e.message);
