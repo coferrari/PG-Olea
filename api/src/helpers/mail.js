2b11c0a38ccf15ac28537832d66379da6b34e6b5
tomasgoyret <tomas.goyret@gmail.com>
2021-10-15 17:15:02 -0300

    arreglos para que quede bien

--- api/src/helpers/mail.js
+++ api/src/helpers/mail.js
@@ -161,7 +161,7 @@ module.exports = {
   getTemplateAdminChangePassword,
   getTemplateAproved,
   getTemplateAuthenticationAdmin,
-  getTemplateProductStock
+  getTemplateProductStock,
   getTemplateRejected,
   getTemplateCategoryLetter,
 };
