ba7c7ee2fcfa6c35a4278d2629a0dc60a0b9d899
Joakingxx <joaquinjoby@gmail.com>
2021-10-16 20:14:45 -0300

    Events mapa a la hora de buscar

--- client/src/components/Admin/Stores/Stores.jsx
+++ client/src/components/Admin/Stores/Stores.jsx
@@ -5,23 +5,28 @@ import Map from "../../Map/Map";
 import { getStores } from "../../../redux/actions/index";
 import axios from "axios";
 import { STORES_URL } from "../../../consts";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  MapConsumer,
+} from "react-leaflet";
 import style from "../Stores/Stores.module.css";
 function Stores() {
   const dispatch = useDispatch();
   const stores = useSelector((state) => state.storesReducer.stores);
   const [address, setAddress] = useState("");
-  const [location, setLocation] = useState([]);
-  const [geocode] = useState(
-    "https://api.geoapify.com/v1/geocode/search?text=${}&format=json&apiKey=7cdeaa9562f54434850b50b6174ba3e0"
-  );
+  const [position, setPosition] = useState([]);
+
   useEffect(() => {
     dispatch(getStores());
-  }, [dispatch, onclick]);
+  }, [dispatch, position]);
   const onChangeAddress = (e) => {
     e.preventDefault();
     setAddress(e.target.value);
   };
+
   const onClickAddress = (e) => {
     e.preventDefault();
     axios
@@ -30,10 +35,11 @@ function Stores() {
       )
       .then((response) => {
         const pos = response.data.results[0];
-        setLocation([pos.lat, pos.lon]);
+        setPosition([pos.lat, pos.lon]);
       })
       .catch((error) => error);
   };
+
   return (
     <div>
       {stores.length === 0 ? (
@@ -66,12 +72,12 @@ function Stores() {
                 </InputGroup>
               </Form.Group>
             </Form>
-            {location.length === 0 ? (
+            {position.length === 0 ? (
               <h3>Por favor ingrese una direccion</h3>
             ) : (
               <div id="mapid" className={style.map}>
                 <MapContainer
-                  center={location}
+                  center={position}
                   zoom={15}
                   scrollWheelZoom={false}
                   className={style.map}
@@ -80,7 +86,7 @@ function Stores() {
                     attribution='<a  href="https://www.google.com/maps/dir//-37.4581198,-61.9376018/@-37.4580681,-62.0078425,12z">OpenGoogleMaps</a>'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   />
-                  <Marker position={location}>
+                  <Marker position={position}>
                     <Popup>
                       Sucursal de <br />
                       Olea en
@@ -94,34 +100,81 @@ function Stores() {
           </div>
         </div>
       ) : (
-        stores.map((e) => {
-          return (
-            <div>
-              <label>{e.address}</label>
-              <Button
-                onClick={async () => {
-                  await axios.delete(STORES_URL + "/" + e.id);
-                  await dispatch(getStores());
-                }}
-              >
-                x
-              </Button>
-              <Map position={e.location} address={e.address} />
+        <div className="container">
+          {stores.map((e) => {
+            return (
               <div>
+                <label>{e.address}</label>
+                <Button
+                  onClick={async () => {
+                    await axios.delete(STORES_URL + "/" + e.id);
+                    await dispatch(getStores());
+                  }}
+                >
+                  x
+                </Button>
+                <Map position={e.location} address={e.address} />
+              </div>
+            );
+          })}
+          <div className="container">
+            <Form onSubmit={(e) => e.preventDefault()}>
+              <Form.Group className="mb-3">
+                <Form.Label>Ingrese la direccion de la Sucursal</Form.Label>
+              </Form.Group>
+              <Form.Group className="mb-3">
                 <InputGroup className="mb-3">
                   <FormControl
-                    placeholder="Recipient's username"
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
+                    placeholder="Ingrese una direccion"
+                    onChange={(e) => {
+                      onChangeAddress(e);
+                    }}
                   />
-                  <Button variant="outline-secondary" id="button-addon2">
+
+                  <Button
+                    onClick={async (e) => {
+                      await onClickAddress(e);
+                    }}
+                  >
                     Button
                   </Button>
                 </InputGroup>
+              </Form.Group>
+            </Form>
+            {position.length === 0 ? (
+              <h3>Por favor ingrese una direccion</h3>
+            ) : (
+              <div id="mapid" className={style.map}>
+                <MapContainer
+                  center={position}
+                  zoom={15}
+                  scrollWheelZoom={false}
+                  className={style.map}
+                >
+                  <MapConsumer>
+                    {(map) => {
+                      map.flyTo(position);
+                      map.zoom = 15;
+                      return null;
+                    }}
+                  </MapConsumer>
+                  <TileLayer
+                    attribution='<a  href="https://www.google.com/maps/dir//-37.4581198,-61.9376018/@-37.4580681,-62.0078425,12z">OpenGoogleMaps</a>'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={position}>
+                    <Popup>
+                      Sucursal de <br />
+                      Olea en
+                      <br />
+                      {address}
+                    </Popup>
+                  </Marker>
+                </MapContainer>
               </div>
-            </div>
-          );
-        })
+            )}
+          </div>
+        </div>
       )}
     </div>
   );
