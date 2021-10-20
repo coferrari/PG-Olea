d9d6949b15fb04783fd2ef7d33e8b057c45f3637
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-20 15:35:49 -0300

    :

--- api/json/turnos.js
+++ api/json/turnos.js
@@ -3,83 +3,83 @@ const { Turn } = require("../src/db");
 const turnMockUp = async () => {
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "10:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "11:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "12:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "13:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "14:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "15:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "17:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-20",
+    date: "2021-10-22",
     hour: "16:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "10:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "11:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "12:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "13:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "14:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "15:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "17:00 hs",
   });
   await Turn.create({
     store: "Garibaldi 283 Coronel Suarez",
-    date: "2021-10-21",
+    date: "2021-10-23",
     hour: "16:00 hs",
   });
 };
-module.exports = storesMockUp;
+module.exports = turnMockUp;
