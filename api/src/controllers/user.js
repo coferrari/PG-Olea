7e896e1cac5122f58dcedb770ba73371053b1f64
dylangavilan <dylan.gavilan32@gmail.com>
2021-10-21 12:06:09 -0300

    .

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -237,10 +237,10 @@ userFunction.googleLogin = async (req, res, next) => {
     if (user) {
       const token = jwt.sign(
         {
-          name: given_name,
-          username: email,
-          email: email,
-          surname: family_name,
+          name: user.name,
+          username: user.username,
+          email: user.email,
+          surname: user.surname,
           picture: user.picture,
           admin: user.admin,
           newsLetter: user.newsLetter,
